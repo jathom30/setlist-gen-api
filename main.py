80568c6bf92cf3c83d56855a164d52c4b885c0f6
jathom30 <jathom30@gmail.com>
2022-04-20 23:06:21 -0400

    song type defined

--- main.py
+++ main.py
@@ -1,7 +1,63 @@
+from contextlib import nullcontext
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional, List
+from enum import Enum
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-  return {"Hello": "world"}
+
+class Tempo(Enum):
+    BALLAD = 'ballad'
+    CHILL = 'chill'
+    MEDIUM = 'medium'
+    UP = 'up'
+    BURNER = 'burner'
+
+
+class Feel(Enum):
+    COUNTRY = 'country'
+    LATIN = 'latin'
+    SWING = 'swing'
+    BLUES = 'blues'
+    ROCK = 'rock'
+    FUNK = 'funk'
+    OTHER = 'other'
+
+
+class SongPlacement(Enum):
+    OPENER = 'opener'
+    CLOSER = 'closer'
+    OTHER = 'other'
+
+
+class Song(BaseModel):
+    name: str
+    tempo: Tempo
+    feel: List[Feel]
+    placement: SongPlacement
+    length: int
+    isCover: Optional[bool] = False
+    exclude: Optional[bool] = False
+    key: Optional[str] = nullcontext
+    notes: Optional[str] = nullcontext
+
+
+@app.get('/songs')
+def get_songs():
+    return {'data': 'song list'}
+
+
+@app.get('/songs/{id}')
+def get_song(id: str):
+    return {'data': id}
+
+
+@app.post('/songs')
+def post_song(request: Song):
+    return request
+
+
+@app.delete('/songs/{id}')
+def delete_song(id: str):
+    return {'data': id}
